8a59a225a740f4947c625e500d17d7542c6a3da8
Matheus Ned <matheusned15@gmail.com>
2022-12-27 17:11:57 -0300

    First commit

---
+++ src/main/java/com/example/mylol/MylolApplication.java
@@ -0,0 +1,13 @@
+package com.example.mylol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MylolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MylolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mylol/MylolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mylol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MylolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
