3a57ab95300f23bff5d2169eb9f9644bda95e01a
Matheus Ned <matheusned15@gmail.com>
2023-07-16 20:35:41 -0300

    creation of constants

---
+++ src/main/java/com/example/mylol/constants/FileConstants.java
@@ -0,0 +1,7 @@
+package com.example.mylol.constants;
+
+public interface FileConstants {
+
+    String SUFFIX_JPG = "_0.jpg";
+    String SUFFIX_PNG = ".png";
+}
---
+++ src/main/java/com/example/mylol/constants/HeaderConstants.java
@@ -0,0 +1,12 @@
+package com.example.mylol.constants;
+
+public interface HeaderConstants {
+
+    String ACCEPT = "Accept";
+    String API_KEY = "api_key";
+    String LOCALE = "locale";
+    String TAGS = "tags";
+    String BEGIN_INDEX= "beginIndex";
+    String END_INDEX = "endIndex";
+    String DATA_BY_ID = "dataById";
+}
